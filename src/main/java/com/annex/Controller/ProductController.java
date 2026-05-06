57f62e9aea77007fc3b45585c24b50ac9e30e473
Bharath Tanniru <bharathtanniru18@gmail.com>
2026-05-06 17:48:19 +0530

    adding changes for delete and update mapping

--- src/main/java/com/annex/Controller/ProductController.java
+++ src/main/java/com/annex/Controller/ProductController.java
@@ -5,30 +5,46 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.annex.Model.Products;
 import com.annex.dao.ProductDao;
 
-@Controller
+@RestController
 public class ProductController {
     
 	@Autowired
 	private ProductDao pdao;
 	
 	@GetMapping("/products")
-	@ResponseBody
+	
 	public List<Products> getProducts(){
 		
 		System.out.println("welcome to products page");
 		return pdao.findAll();
 	}
 	
+	@DeleteMapping("/deleteproduct/{pid}")
+	public String deleteProduct(@PathVariable("pid") int pid) {
+		Products p = pdao.getOne(pid);
+		pdao.delete(p);
+		return "Your record has been deleted";
+		
+	}
+	
+	@PutMapping("/updateproduct")
+	public Products updateProduct(@RequestBody Products product) {
+		return pdao.save(product);
+	}
 	@GetMapping("/products/{pid}")
-	@ResponseBody
+	
 	public Optional<Products> getProduct(@PathVariable("pid") int pid){
 		
 		
@@ -36,7 +52,7 @@ public class ProductController {
 	}
 	
 	@PostMapping("/saveproduct")
-	@ResponseBody
+	
 	public Products saveProduct(Products product){
 		
 		
